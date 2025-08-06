75f32007cb2a46125a56e3b436c2dfabeea2334f
apope8 <apope8@gmail.com>
2025-08-06 16:53:24 +0100

    Spring Guru AI - Direct open ai to respond with Json

--- src/main/java/com/apope/spring_ai_intro/service/OpenAIServiceImpl.java
+++ src/main/java/com/apope/spring_ai_intro/service/OpenAIServiceImpl.java
@@ -3,10 +3,14 @@ package com.apope.spring_ai_intro.service;
 import com.apope.spring_ai_intro.model.Answer;
 import com.apope.spring_ai_intro.model.GetCapitalRequest;
 import com.apope.spring_ai_intro.model.Question;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
@@ -28,6 +32,8 @@ public class OpenAIServiceImpl implements OpenAIService {
     @Value("classpath:templates/get-capital-with-info.st")
     Resource getCapitalPromptWithInfo;
 
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Override
     public String getAnswer(String question) {
@@ -56,7 +62,18 @@ public class OpenAIServiceImpl implements OpenAIService {
         Prompt prompt = promptTemplate.create(Map.of("stateOrCountry", getCapitalRequest.stateOrCountry()));
         ChatResponse response = chatModel.call(prompt);
 
-        return new Answer(response.getResult().getOutput().getText());
+        System.out.println(response.getResult().getOutput().getText());
+        String responseString;
+
+        try{
+            JsonNode jsonNode = objectMapper.readTree(response.getResult().getOutput().getText());
+            responseString = jsonNode.get("answer").asText();
+        }
+        catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new Answer(responseString);
     }
 
     @Override
